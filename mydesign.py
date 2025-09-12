96c31b11143eab4601a99356bd51419389abbc8e
MihirBafna <mihir.bafna@gmail.com>
2025-09-11 20:55:21 -0400

    twomotif twostates twoligands

--- mydesign.py
+++ mydesign.py
@@ -12,84 +12,94 @@ import os
 import argparse
 from utils import motif_utils
 
-parser = argparse.ArgumentParser()
-# parser.add_argument('--len', type=int, default=100)
-# parser.add_argument('--smiles', type=str,default=None)
-parser.add_argument('--num_designs',type= int, default = 1)
-# parser.add_argument('--motif', type=str, default="3ixt")
-parser.add_argument("--motifs", nargs="+", required=True, help="motif names (e.g. 4jhw 1ycr)")
-parser.add_argument('-o','--outpath', type=str, default = "./out/")
-parser.add_argument("--task", required=True, choices=TASK_REGISTRY.keys(), help="task to run")
-args = parser.parse_args()
-
 device = "cuda"
 
-
-# boltz setup
-predict_args={
-    "recycling_steps": 0,
-    "sampling_steps": 200,
-    "diffusion_samples": 1,
-    "write_confidence_summary": True,
-    "write_full_pae": True,
-    "write_full_pde": True,
-}
-diffusion_params = BoltzDiffusionParams()
-diffusion_params.step_scale = 1.638  # Default value
-boltz_model = Boltz1.load_from_checkpoint(
-    os.path.join(os.environ['HOME'],".boltz/boltz1_conf.ckpt"),
-    strict=False,
-    predict_args=predict_args,
-    map_location=device,
-    diffusion_process_args=asdict(diffusion_params),
-    ema=False,
-    structure_prediction_training=True,
-    no_msa=False,
-    no_atom_encoder=False,
-).eval().requires_grad_(False)
-
-out_dir = os.path.join(args.outpath, args.task, "_".join(args.motifs))
-os.makedirs(out_dir, exist_ok=True)
-
-# design
-for design in range(args.num_designs):
-    print(f"\nStarting  {args.task} design {design+1}/{args.num_designs} for motif {args.motifs}")
+def _init_boltz():
+    predict_args={
+        "recycling_steps": 0,
+        "sampling_steps": 200,
+        "diffusion_samples": 1,
+        "write_confidence_summary": True,
+        "write_full_pae": True,
+        "write_full_pde": True,
+    }
+    diffusion_params = BoltzDiffusionParams()
+    diffusion_params.step_scale = 1.638  # Default value
+    boltz_model = Boltz1.load_from_checkpoint(
+        os.path.join(os.environ['HOME'],".boltz/boltz1_conf.ckpt"),
+        strict=False,
+        predict_args=predict_args,
+        map_location=device,
+        diffusion_process_args=asdict(diffusion_params),
+        ema=False,
+        structure_prediction_training=True,
+        no_msa=False,
+        no_atom_encoder=False,
+    ).eval().requires_grad_(False)
     
-    # init task
-    # motif = get_motif(f'motifs/{args.motif}.pdb')
-    motif_templates = motif_utils.get_motif_scaffold_templates([f"motifs/{m}.pdb" for m in args.motifs])
-    # breakpoint()
-    ligand = 'Fc1c(Cl)ccc(n2cnnn2)c1c1c[n+]([O-])c(cc1)C(CC1CC1)n1cc(cn1)c1ccc(N)nc1C'
-    designer = TASK_REGISTRY[args.task](*motif_templates, ligand, length=len(motif_templates[0]['motif_mask']))
-    
-    t0 = time.perf_counter()
-    print("Optimizing sequence...")
-    designer.optimize(boltz_model)
-    t1 = time.perf_counter()
-    print(f"Optimization done in {t1 - t0:.1f} sec")
+    return boltz_model
 
-    print("Saving structures...")
-    t2 = time.perf_counter()
-    structs = designer.get_final_structs(boltz_model)
-    t3 = time.perf_counter()
-    print(f"Structure generation took {t3 - t2:.1f} sec")
-    
-    design_dir = os.path.join(out_dir, f"design{design}")
-    os.makedirs(design_dir, exist_ok=True)
-    
-    for i, motif in enumerate(args.motifs):
-        with open(os.path.join(design_dir,f"{motif}_spec.pkl"), "wb") as f:    # also save motifspec for eval
-            pickle.dump(motif_templates[i], f)
+def run(args):
+    # boltz setup
+    boltz_model = _init_boltz()
+
+    out_dir = os.path.join(args.outpath, "_".join(args.motifs))
+    os.makedirs(out_dir, exist_ok=True)
+
+    # design
+    for design in range(args.num_designs):
+        print(f"\nStarting  {args.task} design {design+1}/{args.num_designs} for motifs {args.motifs} and ligands {args.ligands}")
         
-    for output, struct_list, state_idx in structs:
-        with open(os.path.join(design_dir, f"state{state_idx}.pkl"), "wb") as f:
-            pickle.dump(output, f)
+        # init task
+        # motif = get_motif(f'motifs/{args.motif}.pdb')
+        motif_templates = motif_utils.get_motif_scaffold_templates([f"motifs/{m}.pdb" for m in args.motifs])
+        # breakpoint()
+        # ligand = 'Fc1c(Cl)ccc(n2cnnn2)c1c1c[n+]([O-])c(cc1)C(CC1CC1)n1cc(cn1)c1ccc(N)nc1C'
 
-        for j, struct in enumerate(struct_list):
-            base = f"state{state_idx}_sample{j}"
-            with open(os.path.join(design_dir, base + ".pdb"), "w") as f:
-                f.write(to_pdb(struct))
-            with open(os.path.join(design_dir, base + ".cif"), "w") as f:
-                f.write(to_mmcif(struct))
-                
-    print(f"Finished design {motif} {design+1} in {t3 - t0:.1f} sec total")
+        designer = TASK_REGISTRY[args.task](motifs=motif_templates, ligands=args.ligands, length=len(motif_templates[0]['motif_mask']))
+        
+        t0 = time.perf_counter()
+        print("Optimizing sequence...")
+        designer.optimize(boltz_model)
+        t1 = time.perf_counter()
+        print(f"Optimization done in {t1 - t0:.1f} sec")
+
+        print("Saving structures...")
+        t2 = time.perf_counter()
+        structs = designer.get_final_structs(boltz_model)
+        t3 = time.perf_counter()
+        print(f"Structure generation took {t3 - t2:.1f} sec")
+        
+        design_dir = os.path.join(out_dir, f"design{design}")
+        os.makedirs(design_dir, exist_ok=True)
+        
+        for i, motif in enumerate(args.motifs):
+            with open(os.path.join(design_dir,f"{motif}_spec.pkl"), "wb") as f:    # also save motifspec for eval
+                pickle.dump(motif_templates[i], f)
+            
+        for output, struct_list, state_idx in structs:
+            with open(os.path.join(design_dir, f"state{state_idx}.pkl"), "wb") as f:
+                pickle.dump(output, f)
+
+            for j, struct in enumerate(struct_list):
+                base = f"state{state_idx}_sample{j}"
+                with open(os.path.join(design_dir, base + ".pdb"), "w") as f:
+                    f.write(to_pdb(struct))
+                with open(os.path.join(design_dir, base + ".cif"), "w") as f:
+                    f.write(to_mmcif(struct))
+                    
+        print(f"Finished design {args.motifs} {design+1} in {t3 - t0:.1f} sec total")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('--len', type=int, default=100)
+    parser.add_argument('--num_designs',type= int, default = 1)
+    parser.add_argument("--motifs", nargs="+", required=True, help="motif names (e.g. 4jhw 1ycr)")
+    parser.add_argument('--ligands', nargs="+",default=["Fc1c(Cl)ccc(n2cnnn2)c1c1c[n+]([O-])c(cc1)C(CC1CC1)n1cc(cn1)c1ccc(N)nc1C"] ,help="space separated ligand smiles")
+    parser.add_argument('-o','--outpath', type=str, default = "./out/")
+    parser.add_argument("--task", required=True, choices=TASK_REGISTRY.keys(), help="task to run")
+    args = parser.parse_args()
+
+    
+    run(args)
--- task.py
+++ task.py
@@ -12,7 +12,7 @@ def task(name):
 
 
 @task('onemotif_twostates_neg')
-def onemotif_twostates_neg(motif, ligand, length):
+def onemotif_twostates_neg(motifs, ligands, length):
     """
     Sequence folds into two structure states with negative allostery: 
     - State 0: motif active when ligand unbound
@@ -20,15 +20,15 @@ def onemotif_twostates_neg(motif, ligand, length):
     """
     
     designer = MultistateDesigner(num_states=2)
-    designer.add_motif(motif, state=0)
-    designer.add_anti_motif(motif, state=1)
-    designer.add_ligand(ligand, state=1)
+    designer.add_motif(motifs[0], state=0)
+    designer.add_anti_motif(motifs[0], state=1)
+    designer.add_ligand(ligands[0], state=1)
     designer.initialize(length=length)
     
     return designer
 
 @task('onemotif_twostates_pos')
-def onemotif_twostates_pos(motif, ligand, length):
+def onemotif_twostates_pos(motifs, ligands, length):
     """
     Sequence folds into two structure states with positive allostery: 
     - State 0: motif inactive when ligand unbound
@@ -36,30 +36,32 @@ def onemotif_twostates_pos(motif, ligand, length):
     """
     
     designer = MultistateDesigner(num_states=2)
-    designer.add_anti_motif(motif, state=0)
-    designer.add_motif(motif, state=1)
-    designer.add_ligand(ligand, state=1)
+    designer.add_anti_motif(motifs[0], state=0)
+    designer.add_motif(motifs[0], state=1)
+    designer.add_ligand(ligands[0], state=1)
     designer.initialize(length=length)
     
     return designer
 
 
 @task('twomotif_twostates')
-def twomotif_twostates(motifA,motifB, ligand, length):
+def twomotif_twostates(motifs, ligands, length):
     """
     Sequence folds into two structure states switching between two motifs: 
     - State 0: motif B inactive and motif A active when ligand unbound
     - State 1: motif B active and motif A inactive when ligand bound
     """
-    
+    motifA,motifB = motifs[0],motifs[1]
+
     designer = MultistateDesigner(num_states=2)
-    
     designer.add_anti_motif(motifB, state=0)
     designer.add_motif(motifA, state=0)
+    if len(ligands) == 2:
+        designer.add_ligand(ligands[1], state=0)
     
     designer.add_anti_motif(motifA, state=1)
     designer.add_motif(motifB, state=1)
-    designer.add_ligand(ligand, state=1)
+    designer.add_ligand(ligands[0], state=1)
     
     designer.initialize(length=length)
     
