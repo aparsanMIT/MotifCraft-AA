70943480e033c220f02482f9b5df3db3d97589db
MihirBafna <mihir.bafna@gmail.com>
2025-09-14 21:29:54 -0400

    zinc negative allostery one motif

--- scripts/run_motif_designs.sh
+++ scripts/run_motif_designs.sh
@@ -4,25 +4,27 @@
 
 
 num_designs=100
-task=onemotif_twostates_pos
+task=onemotif_twostates_neg
 
 visible_devices_list=(0 1 2 3 4 5 6 7)   # gpus available on thes machine
 
  # motifs to design
-# motifs=(1bcf 1prw 1qjg 1ycr 2kl8 4jhw 4zyp)                      
+# motifs=(1bcf 1prw 1qjg 1ycr 2kl8 3ixt 4jhw 4zyp)                      
 # motifs=(5ius 5tpn 5trv_long 5trv_med 5trv_short 5wn9 5yui 6e6r_long)     
 motifs=(6e6r_med 6e6r_short 6exz_long 6exz_med 6exz_short 7mrx_60 7mrx_85 7mrx_128)
 
 # ligands for allostery
 ligands='[Zn+2]'
 
-outdir=./out/onemotif_twostates_pos_zn
+outdir=./out/onemotif_twostates_neg_zn
 
 conda_env=boltz_design
 home_override=/data/cb/mihirb14/bin
 
+echo "task: $task"
 echo "motifs: [${motifs[@]}]"
 echo "ligands: [\"$ligands\"]"
+echo "outdir: $outdir"
 
 for i in "${!motifs[@]}"; do
     motif="${motifs[$i]}"
