f0ec25a280aefd4c2e4bcf771a8907b0747fcc78
MihirBafna <mihir.bafna@gmail.com>
2025-09-19 14:35:35 -0400

    fad pos allostery

--- scripts/run_motif_designs.sh
+++ scripts/run_motif_designs.sh
@@ -4,7 +4,7 @@
 
 
 num_designs=100
-task=onemotif_twostates_neg
+task=onemotif_twostates_pos
 
 visible_devices_list=(0 1 2 3 4 5 6 7)   # gpus available on thes machine
 
@@ -14,9 +14,10 @@ motifs=(5ius 5tpn 5trv_long 5trv_med 5trv_short 5wn9 5yui 6e6r_long)
 # motifs=(6e6r_med 6e6r_short 6exz_long 6exz_med 6exz_short 7mrx_60 7mrx_85 7mrx_128)
 
 # ligands for allostery
-ligands='[Mg+2]'
+# ligands='[Mg+2]'
+ligands='CC1=CC2=C(C=C1C)N(C3=NC(=O)NC(=O)C3=N2)C[C@@H]([C@@H]([C@@H](COP(=O)(O)OP(=O)(O)OC[C@@H]4[C@H]([C@H]([C@@H](O4)N5C=NC6=C(N=CN=C65)N)O)O)O)O)O'
 
-outdir=./out/onemotif_twostates_neg_mg
+outdir=./out/onemotif_twostates_pos_FAD
 
 conda_env=boltz_design
 home_override=/data/cb/mihirb14/bin
